e40dac54c93eff200d05b25c835ccca4bc578554
kovalyovaov <97628587+kovalyovaov@users.noreply.github.com>
2022-04-26 19:13:54 +0300

    push

--- js/js.js
+++ js/js.js
@@ -1,2 +1,35 @@
-const name = 1;
-console.log (name);
+
+// function filterArray(numbers, value) {
+//     // Change code below this line
+//  const newArray = [];
+//  for (let i = 0; i < numbers.length; i +=1) {
+//    console.log(numbers[i]);
+//    if (numbers[i]>value) {
+//     newArray.push(numbers[i]);
+   
+//    }
+
+//  }
+//  console.log (newArray);
+// return newArray;
+
+//    // Change code above this line
+//  }
+//  filterArray([1, 20, 10, 4, 5], 5);
+
+
+// 
+
+function getEvenNumbers(start, end) {
+    // Change code below this line
+ newArray = [];
+ 
+ for (let i=start; i<=end; i+=1 ){
+   if (i%2===0){
+     newArray.push(i);
+   }
+ }
+ return;
+ 
+     // Change code above this line
+   }
