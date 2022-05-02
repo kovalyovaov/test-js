f0b01b2a77a2fd9010df05291f4424164f3127e5
kovalyovaov <97628587+kovalyovaov@users.noreply.github.com>
2022-05-02 09:44:27 +0300

    push

--- js/js.js
+++ js/js.js
@@ -32,13 +32,32 @@
 // const rezalt = number >nsme1 && number < nsme2;
 // console.log (rezalt);
 
-const name = 'Ivan';
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
 
-if (name==='ivan') {
-  console.log ('true');
+function getAllPropValues(propName) {
+  // Change code below this line
+for (const product of products) {
+ 
 
 }
- console.log ('false');
+
+}
+
+getAllPropValues ('dss')
+
+
+
+
+
+
+
+
+
 
 
 
