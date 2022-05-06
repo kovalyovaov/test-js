a50857477309cc32dfe3399972b0508eada18255
kovalyovaov <97628587+kovalyovaov@users.noreply.github.com>
2022-05-06 20:57:51 +0300

    push

--- js/js.js
+++ js/js.js
@@ -31,22 +31,67 @@
 
 // const rezalt = number >nsme1 && number < nsme2;
 // console.log (rezalt);
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for (const potion of this.potions) {
+      console.log(potion);
+      if (newPotion.name === potion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+  },
 
-const bookShelf = {
-  books: ["Последнее королевство", "Страж снов"],
-  // Это метод объекта
-  getBooks() {
-    console.log("Этот метод будет возвращать все книги - свойство books");
+    removePotion(potionName) {
+      for (let i=0; i< this.potions.length; i+=1) {
+        if (potionName === this.potions[i].name){
+          return this.potions.splice(i,1);
+        }
+        return `Potion ${potionName} is not in inventory!`;  
+      }},
+      
+
+  updatePotionName(oldName, newName) {
+    let potionIndex=-1;
+    for (let potion of this.potions) {
+      console.log("ddddd", potion.name)
+      if (potion.name === oldName){
+        
+         potion.name = newName;
+         potionIndex+=1;
+         
+      }
+      if (potionIndex===-1){
+      return `Potion ${oldName} is not in inventory!`;}
+      //  (this.potions)
+
+    }
+    // const potionIndex = this.potions.indexOf(oldName);
+
+    // if (potionIndex === -1) {
+    //   return `Potion ${oldName} is not in inventory!`;
+    // }
+
+    // this.potions.splice(potionIndex, 1, newName);
   },
-  // Это метод объекта
-  addBook(bookName) {
-    console.log("Этот метод будет добавлять новую книгу в свойство books");
-  },
+ 
 };
+// console.table(atTheOldToad.getPotions());
+console.log(atTheOldToad.updatePotionName("Speed potion", "ffffff"));
+console.table(atTheOldToad.getPotions());
+  
 
-// Вызовы методов
-bookShelf.getBooks();
-bookShelf.addBook("Новая книга");
+
+
 
 
 
