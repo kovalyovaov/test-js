a2870a3a9b9c16e728f50a27f55e3fba7b2cd210
kovalyovaov <97628587+kovalyovaov@users.noreply.github.com>
2022-05-27 20:37:51 +0300

    push

--- js/indexOf.js
+++ js/indexOf.js
@@ -1,3 +1,19 @@
-const getFriends = (users) =>
-users.flatMap(user=>user.friends).
-filter((friend, index, array)=>array.indexOf(friend)===index);
+class StringBuilder {
+    constructor (initialValue){
+        this.value = initialValue;
+    }
+    getValue(){
+        return this.value;
+    }
+    
+}
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
